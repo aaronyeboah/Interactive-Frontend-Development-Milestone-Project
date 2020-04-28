0df88c7da8654ec8a8c486b509f9ee631e274482
aaronyeboah <a.m.yeboah@hotmail.com>
2020-04-28 03:47:28 +0000

    Adding flight search widget

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -9,10 +9,16 @@ xhr.addEventListener("readystatechange", function () {
 	}
 });
 
-xhr.open("GET", "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/?destination=LED&origin=MOW");
-xhr.setRequestHeader("x-rapidapi-host", "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com");
+xhr.open("GET", "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm");
+xhr.setRequestHeader("x-rapidapi-host", "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com");
 xhr.setRequestHeader("x-rapidapi-key", "2b1daa7d01msh975f208f11d0872p135fa2jsnf0d45637b825");
 
 xhr.send(data);
+
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementsById("data").innerHTML = data;
+    });
+}
 
 $('.carousel').carousel()
